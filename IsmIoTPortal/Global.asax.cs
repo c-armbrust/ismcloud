71ecf1634ae552d6aa50b2470657d41b88aacabd
Leo <leoreentry@protonmail.com>
2016-09-26 13:19:48 +0000

    Add SSL Support and force HTTPS Everywhere. The redirect from HTTP to HTTPS doesn't work on the Development Server since our Development Port for SSL is 44338.

--- IsmIoTPortal/Global.asax.cs
+++ IsmIoTPortal/Global.asax.cs
@@ -13,7 +13,6 @@ namespace IsmIoTPortal
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalFilters.Filters.Add(new RequireHttpsAttribute());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
