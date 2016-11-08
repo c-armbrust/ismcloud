6ec62ab5b35d0f7090ba5c43cde37337b856c283
Leo <leoreentry@protonmail.com>
2016-11-08 09:46:59 +0000

    Add basic Web API for device to register.

---
+++ IsmIoTPortal/Controllers/NewDeviceController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IsmIoTPortal.Models;
+using IsmIoTSettings;
+using Newtonsoft.Json;
+
+namespace IsmIoTPortal.Controllers
+{
+    [AllowAnonymous]
+    public class NewDeviceController : ApiController
+    {
+        private readonly IsmIoTPortalContext db = new IsmIoTPortalContext();
+        public dynamic Get()
+        {
+            var locations = db.Locations.Select(i =>
+                    new {i.LocationId, i.City, i.Country});
+            var hardwares = db.Hardware.Select(i =>
+                    new {i.HardwareId, i.Board, i.Camera});
+            var softwares = db.Software.Select(i =>
+                    new { i.SoftwareId, i.SoftwareVersion });
+            return new 
+            {
+                Locations = locations,
+                Hardware = hardwares,
+                Software = softwares
+            };
+        }
+
+        public object Get(string id)
+        {
+            if (RegexHelper.Text.IsMatch(id))
+            {
+                var response = new
+                {
+                    id = id,
+                    guid = System.Web.Security.Membership.GeneratePassword(20, 0),
+
+                };
+                return response;
+            }
+            return new string[]
+            {
+             "Hello",
+             "World"
+            };
+        }
+
+    }
+}
---
+++ IsmIoTPortal/Models/NewDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IsmIoTPortal.Models
+{
+    public class NewDevice
+    {
+        public int IsmDeviceId { get; set; }
+        public string DeviceId { get; set; }
+        public int LocationId { get; set; }
+        public int SoftwareId { get; set; }
+        public int HardwareId { get; set; }
+        public string PASS { get; set; }
+    }
+}
