80e5874dd86e8be9f6d2ff9b694a84336ae969d8
Leo <leoreentry@protonmail.com>
2017-02-08 16:33:42 +0100

    Add Settings.cs

---
+++ IsmIoTSettings/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsmIoTSettings
+{
+    public class Settings
+    {
+        // 
+        // ******** LOCAL ********
+#if DEBUG
+        public static string webDomain = "localhost";
+        public static string webProtocol = "https";
+        public static string webPort = "44338";
+        public static string webCompleteAddress = webProtocol + "://" + webDomain + ":" + webPort;
+#else
+        public static string webDomain = "ismportal.azurewebsites.net";
+        public static string webProtocol = "https";
+        public static string webPort = "";
+        public static string webCompleteAddress = webProtocol + "://" + webDomain;
+#endif
+
+   }
+}
